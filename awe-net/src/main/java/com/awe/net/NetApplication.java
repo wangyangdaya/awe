e64f87754b04fec100835253a91ccf3439bd5b66
wangy <wangyang_daya@126.com>
2018-07-27 14:49:01 +0800

    mybatisPlus

--- awe-net/src/main/java/com/awe/net/NetApplication.java
+++ awe-net/src/main/java/com/awe/net/NetApplication.java
@@ -1,7 +1,9 @@
 package com.awe.net;
 
+import com.awe.net.service.DiscardClient;
 import com.awe.net.service.DiscardServer;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -14,18 +16,32 @@ import org.springframework.context.annotation.Bean;
  * <p>Created by admin on 2018/7/20.</p>
  */
 @SpringBootApplication
-public class NetApplication {
+public class NetApplication  implements CommandLineRunner {
 
     public static void main(String[] args) {
 
         SpringApplication.run(NetApplication.class, args);
     }
 
-//    @Bean
+    @Bean
     public InitializingBean initializingBean() {
         return() -> {
             DiscardServer discardServer = new DiscardServer();
             discardServer.start(8889);
         };
+    }
+
+//    @Bean
+//    public InitializingBean initializingClient() {
+//        return() -> {
+//            DiscardClient discardClient = new DiscardClient();
+//            discardClient.connect("127.0.0.1", 8889);
+//        };
+//    }
+
+    @Override
+    public void run(String... args) throws Exception {
+//        DiscardServer discardServer = new DiscardServer();
+//        discardServer.start(8889);
     }
 }
